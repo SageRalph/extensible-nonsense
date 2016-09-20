eeab65f94660b68c535f9d38398778a1a49a0d8f
David Ralph <David.M.L.Ralph@gmail.com>
2016-09-21 00:13:17 +0100

    Changed routes
    Moved /api to /

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const engine = require('../services/engine');
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', engine.getFromRandomBranch);
+router.post('/', engine.postToBranch);
+
+router.get('/all', engine.getAllBranches);
+router.delete('/', engine.reset);
 
 module.exports = router;
